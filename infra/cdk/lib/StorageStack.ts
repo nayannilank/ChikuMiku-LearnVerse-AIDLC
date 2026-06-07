d4fb733aca7a0d9d065ea8aa9170e01e366b0a6e
Nayan Nilank <nilankn@amazon.com>
2026-06-07 22:59:27 +0530

    feat: Move web hosting to Vercel, remove CloudFront/S3 web hosting
    - Add vercel.json with Vite framework config and SPA rewrites
    - Remove CloudFront distribution and web app S3 bucket from StorageStack
    - Keep content S3 bucket for Lambda file storage
    - Simplify deploy.yml to only deploy CDK backend (API + Lambda + DB)
    - Remove CloudFront-related tests from StorageStack.test.ts
    
    Web app will be hosted on Vercel (free tier) instead of
    CloudFront + S3 (which incurs costs).

--- infra/cdk/lib/StorageStack.ts
+++ infra/cdk/lib/StorageStack.ts
@@ -1,7 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { Construct } from 'constructs';
 
 export interface StorageStackProps extends cdk.NestedStackProps {
@@ -10,8 +8,6 @@ export interface StorageStackProps extends cdk.NestedStackProps {
 
 export class StorageStack extends cdk.NestedStack {
   public readonly contentBucket: s3.Bucket;
-  public readonly webAppBucket: s3.Bucket;
-  public readonly distribution: cloudfront.Distribution;
 
   constructor(scope: Construct, id: string, props: StorageStackProps) {
     super(scope, id, props);
@@ -19,7 +15,7 @@ export class StorageStack extends cdk.NestedStack {
     const { stageName } = props;
     const isProd = stageName === 'prod';
 
-    // Content Bucket
+    // Content Bucket (for textbook page images and uploaded content)
     this.contentBucket = new s3.Bucket(this, 'ContentBucket', {
       bucketName: `learnverse-${stageName}-content-${cdk.Stack.of(this).account}`,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
@@ -45,53 +41,6 @@ export class StorageStack extends cdk.NestedStack {
         },
       ],
       removalPolicy: isProd ? cdk.RemovalPolicy.RETAIN : cdk.RemovalPolicy.DESTROY,
-    });
-
-    // Web App Bucket
-    this.webAppBucket = new s3.Bucket(this, 'WebAppBucket', {
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: isProd ? cdk.RemovalPolicy.RETAIN : cdk.RemovalPolicy.DESTROY,
-    });
-
-    // Origin Access Identity for CloudFront → Web App Bucket
-    const oai = new cloudfront.OriginAccessIdentity(this, 'WebAppOAI', {
-      comment: `OAI for learnverse-${stageName}-web-app`,
-    });
-    this.webAppBucket.grantRead(oai);
-
-    // CloudFront Distribution
-    this.distribution = new cloudfront.Distribution(this, 'WebAppDistribution', {
-      defaultBehavior: {
-        origin: new origins.S3Origin(this.webAppBucket, {
-          originAccessIdentity: oai,
-        }),
-        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-      },
-      httpVersion: cloudfront.HttpVersion.HTTP2_AND_3,
-      defaultRootObject: 'index.html',
-      errorResponses: [
-        {
-          httpStatus: 403,
-          responseHttpStatus: 200,
-          responsePagePath: '/index.html',
-        },
-        {
-          httpStatus: 404,
-          responseHttpStatus: 200,
-          responsePagePath: '/index.html',
-        },
-      ],
-      additionalBehaviors: {
-        '/api/*': {
-          origin: new origins.HttpOrigin('placeholder-api-gateway.execute-api.ap-south-1.amazonaws.com', {
-            protocolPolicy: cloudfront.OriginProtocolPolicy.HTTPS_ONLY,
-          }),
-          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-          cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
-          allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
-          originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
-        },
-      },
     });
   }
 }
